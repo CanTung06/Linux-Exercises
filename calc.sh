3c00643770c5c68d48bf512e3ee51bb8675144e6
CanTung <cantung678@gmail.com>
2026-02-16 20:55:28 +0700

    Bai 20

---
+++ calc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Kiem tra dung 3 tham so
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <a> <op> <b>"
+    exit 1
+fi
+
+a=$1
+op=$2
+b=$3
+
+case $op in
+    +)
+        result=$(($a+$b));;
+    -)
+        result=$(($a-$b));;
+    m)
+        result=$(($a*$b));;
+    /)
+        if [ "$b" -eq 0 ]; then
+            echo "Khong chia duoc cho 0"
+            exit 1
+        fi
+        result=$(($a/$b));;
+    *)
+        echo "Phep tinh khong hop le"
+        exit 1;;
+esac
+
+if [ "$2" -ne "m" ]; then
+    echo "$a $op $b = $result"
+else
+    echo "$a * $b = $result"
+fi
